6717d154a665263a46f1ec7322f121b335d92c31
cjmoro <scjmoro@gmail.com>
2020-12-10 19:49:09 -0800

    Add store

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import PlacesNavigator from "./navigation/PlacesNavigator";
+import placesReducer from "./store/places-reducer";
+import ReduxThunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+const rootReducer = combineReducers({
+	places: placesReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
-	return <PlacesNavigator></PlacesNavigator>;
+	return (
+		<Provider store={store}>
+			<PlacesNavigator></PlacesNavigator>
+		</Provider>
+	);
 }
