c07caf53da5fb18e26a1b6f0e31250459e166487
cjmoro <scjmoro@gmail.com>
2020-12-13 14:00:18 -0800

    Add store image on filesystem

--- store/places-actions.ts
+++ store/places-actions.ts
@@ -1,5 +1,29 @@
+import * as FileSystem from "expo-file-system";
+import { ThunkAction } from "redux-thunk";
+import { RootState } from "../App";
 import { ADD_PLACE, PlacesActionTypes } from "./types";
 
-export const addPlace = (title: string, image: string): PlacesActionTypes => {
-	return { type: ADD_PLACE, placeData: { title, image } };
+export const addPlace = (
+	title: string,
+	image: string
+): ThunkAction<void, RootState, unknown, PlacesActionTypes> => {
+	return async (dispatch) => {
+		const fileName = image.split("/").pop() || "";
+		const newPath = FileSystem.documentDirectory
+			? FileSystem.documentDirectory + fileName
+			: `${new Date().toISOString()}.jpg`;
+		try {
+			await FileSystem.moveAsync({
+				from: image,
+				to: newPath,
+			});
+		} catch (e) {
+			console.log(e);
+			throw e;
+		}
+		return dispatch({
+			type: ADD_PLACE,
+			placeData: { title, image: newPath },
+		});
+	};
 };
