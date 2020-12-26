c230a46651dce3ad08c23871c4bdc8978f7c2e13
cjmoro <scjmoro@gmail.com>
2020-12-25 21:23:17 -0800

    Add void type

--- helpers/db.ts
+++ helpers/db.ts
@@ -3,7 +3,7 @@ import * as SQLite from "expo-sqlite";
 const db = SQLite.openDatabase("places.db");
 
 export const init = () => {
-	const promise = new Promise((res, rej) => {
+	const promise = new Promise<void>((res, rej) => {
 		db.transaction((tx) => {
 			tx.executeSql(
 				"CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NOT NULL, address TEXT NOT NULL, lat REAL NOT NULL, lng REAL NOT NULL);",
